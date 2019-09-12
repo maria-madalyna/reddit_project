260007138811484fd2429071da4bad2750851e9c
gabrieloarcas <you@example.com>
2019-09-12 13:27:19 +0100

    latest updates for users, posts, links, messages and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-
-resources :posts
-
-root "posts#index"
+  devise_for :users
+  resources :links
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments
+  end
+  resources :pages
+  root to: "posts#index"
+  
+  # get    '/pages',          to: 'pages#index'
+  # post   '/pages',          to: 'pages#create'
+  # get    '/pages/new',      to: 'pages#new',  as: 'new_page'
+  # get    '/pages/:id',      to: 'pages#show', as: 'page'
+  # get    '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  # patch  '/pages/:id',      to: 'pages#update'
+  # delete '/pages/:id',      to: 'pages#destroy'
 end
