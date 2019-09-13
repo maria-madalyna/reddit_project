d9ec9736491fe03443d8fb6bdd879c0a14e08106
gabrieloarcas <you@example.com>
2019-09-13 18:57:04 +0100

    Added some voting but only into the individual post. Perhaps we'll improve this with ajax.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  respond_to :js, :json, :html
 
   # before_action :set_post, except: [:index, :new, :create]
 
@@ -33,6 +34,18 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     redirect_to posts_path
+  end
+
+  def upvote
+    @post = Post.find(params[:id])
+    @post.upvote_by current_user
+    redirect_to @post #Updating the vote without reloading.......:)) 
+  end
+    
+  def downvote
+    @post = Post.find(params[:id])
+    @post.downvote_by current_user
+    redirect_to @post
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
   belongs_to :category
+  acts_as_votable
 end
 
 # has_many :comments, dependent: :destroy
