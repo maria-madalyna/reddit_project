0efcb0c36c301ea55422abd1c0e53ea88f810c37
gabrieloarcas <you@example.com>
2019-09-13 07:09:11 +0100

    Added username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [email: true, login: false]
+
+attr_writer :login
+
+def login
+  @login || self.username || self.email
+end
   
   has_many :links
   has_many :posts
+
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+ # Only allow letter, number, underscore and punctuation.
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+
+  # validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_112800) do
+ActiveRecord::Schema.define(version: 2019_09_13_052546) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -53,8 +53,10 @@ ActiveRecord::Schema.define(version: 2019_09_12_112800) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
